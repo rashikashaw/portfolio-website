25189e47d3695f93c86460d922b5650d39384887
Rashika Shaw <rashikashaw97@gmail.com>
2024-10-23 03:30:05 -0400

    fixed experience

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -31,20 +31,21 @@ export const AboutSection = () => {
               <div className="my-3">
                 <h2 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-blue-700">Mxpert</h2>
                 <h3 className="text-zinc-500 py-2"> St. Johns, NL</h3>
-                <h3 className="text-zinc-500">Jun 2023 - Present (4 months)</h3>
+                <h3 className="text-zinc-500">Jun 2023 - Present</h3>
               </div>
               <ul className="flex list-outside list-disc flex-col text-darkGrayishBlue">
-                <li className="m-2">
-                Used NodeJS, ORM and SQL/No-SQL to develop and manage databases.
-                </li>
-                <li className="m-2">
-                  Reviewed code, debugged problems, and corrected issues.</li>
-                <li className="m-2">
-                  Build key features of the product. React for frontend, Node for backend and Mongo-DB for database.</li>
-                <li className="m-2">
-                  Features build helped in retaining customers and raised usage to 70%.</li>
-                <li className="m-2">
-                  Boosted network, system and data availability and integrity through preventive maintenance and upgrades.</li>
+                <li className="m-2">Refactoredthecompany’sbackendfromMongoDBwithJavaScript to TypeScript, Node.js, and PostgreSQL using Prisma ORM.</li>
+                <li className="m-2">Designed and implemented relational databases hosted on Amazon RDS.</li>
+                <li className="m-2">Developed REST APIs for efficient data handling and seamless integration across the application.</li>
+                <li className="m-2">Built the frontend with React and TypeScript, creating reusable, scalable components using Shadcn for a seamless user experience.</li>
+                <li className="m-2">Integrated Bland AI into our system as a third-party platform,
+                improving system functionalities and AI-driven features.</li>
+                <li className="m-2">Wrote unit and integration test using Jest.</li>
+                <li className="m-2">Implemented a webhook-based notification system for real-time
+                updates, enhancing responsiveness and user engagement.</li>
+                <li className="m-2">Familiar with AWS platform.</li>
+                <li className="m-2">Technologies used - React, Typescript, PostgreSQL, AWS(ECS,
+                  Amplify, RDS), MySQL, MongoDB, POSTMAN, Git.</li>
               </ul>
             </div>
           </div>
@@ -71,73 +72,14 @@ export const AboutSection = () => {
               </div>
               <ul className="flex list-outside list-disc flex-col text-darkGrayishBlue">
                 <li className="m-2">
-                  Implemented and deployed a platform with a team of 6 that streamlined the process of connecting entrepreneurs with relevant professionals; saved 15+ hours per week in total search and connection time, resulting in increased lead generation by 30%.
+                Working closely with designers to bring mockups and wireframes to life, ensuring the UI matches the design vision
                 </li>
                 <li className="m-2">
-                  Created a comprehensive library of reusable components and streamlined the development process by 20%; led to a 30% reduction in project delivery time.
-                  Build key features of the product. React for frontend, Node for backend and Mongo-DB for database.</li>
+                Utilizing tools like Lighthouse and WebPageTest to improve page load times and ensure performance optimization
+                </li>
                 <li className="m-2">
-                Conducted comprehensive usability tests with 10 users, identified 30+ usability issues and recommended improvements that increased conversion rate by 15%.</li>
-              </ul>
-            </div>
-          </div>
-          {/* list item 3 */}
-          <div className="flex flex-col space-y-3 lg:space-x-6 lg:space-y-0 lg:flex-row">
-            <div className="rounded-full bg-seaGreen lg:bg-transparent">
-              <div className="flex items-center space-x-2">
-                <div className="px-4 py-2 text-white rounded-full lg:py-1 bg-seaGreen">
-                  03
-                </div>
-                <h3 className="text-base font-bold lg:mb-4 lg:hidden">
-                  Business Analyst
-                </h3>
-              </div>
-            </div>
-            <div>
-              <h3 className="hidden mb-4 text-lg font-bold lg:block">
-                Business Analyst
-              </h3>
-              <div className="my-3">
-                <h2 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-blue-700">Dev Launchers</h2>
-                <h3 className="text-zinc-500 py-2">Austin, Texas</h3>
-                <h3 className="text-zinc-500">Sep 2021 - May 2022 (9 months)</h3>
-              </div>
-              <ul className="flex list-outside list-disc flex-col text-darkGrayishBlue">
-                <li className="m-2">Developed business plans, determine Key Performance Indicator (KPI) and coordinate the measurement result.</li>
-                <li className="m-2">Analyzed quantitative and qualitative data to uncover competition and industry information to drive the business revenue.</li>
-                <li className="m-2">Planned and documented key features of the application. </li>
-                <li className="m-2">Held usability tests with existing users and planned changes according to the feedback provided.</li>
-              </ul>
-            </div>
-          </div>
-          {/* list item 4 */}
-          <div className="flex flex-col space-y-3 lg:space-x-6 lg:space-y-0 lg:flex-row">
-            <div className="rounded-full bg-seaGreen lg:bg-transparent">
-              <div className="flex items-center space-x-2">
-                <div className="px-4 py-2 text-white rounded-full lg:py-1 bg-seaGreen">
-                  04
-                </div>
-                <h3 className="text-base font-bold lg:mb-4 lg:hidden">
-                  Business Analyst (internship)
-                </h3>
-              </div>
-            </div>
-            <div>
-              <h3 className="hidden mb-4 text-lg font-bold lg:block">
-                Business Analyst (internship)
-              </h3>
-              <div className="my-3">
-                <h2 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-blue-700">Promena LLP</h2>
-                <h3 className="text-zinc-500 py-2">Bengaluru, Karnataka</h3>
-                <h3 className="text-zinc-500">Dec 2020 - Jun 2021 (7 months)</h3>
-              </div>
-              <ul className="flex list-outside list-disc flex-col text-darkGrayishBlue">
-                <li className="m-2"> Documented clients requirement.</li>
-                <li className="m-2">Used Miro for creating product roadmap and documenting user stories.</li>
-                <li className="m-2">Used JIRA for sprint planning.</li>
-                <li className="m-2">Communicated with development and UX team. Generated business intelligence reports to inform strategic decision-making.</li>
-                <li className="m-2">Collaborated with stakeholders to identify business needs and data sources.</li>
-                <li className="m-2">Performed gap analysis to identify areas of improvement.</li>
+                Implementing features that integrate Large Language Models(LLaMA).</li>
+                <li className="m-2">Develop reusable components using React, TypeScript, and state management with Redux Toolkit.</li>
               </ul>
             </div>
           </div>
