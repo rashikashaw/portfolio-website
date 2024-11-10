1f128431067616c31d714b624577b66a775ad148
Rashika Shaw <rashikashaw97@gmail.com>
2024-11-10 02:11:35 -0500

    changed tags color

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -5,9 +5,9 @@ type TagProp = {
 export const Tags = (props: TagProp) => {
   const { value } = props;
   return (
-    <div className="mt-10 bg-gradient-to-r from-cyan-300 to-cyan-800 p-1 
-    rounded-2xl baseline  dark:bg-gray-800 shadow-md shadow-cyan-900/50 dark:shadow-cyan-400/50">
-      <div className="flex justify-center items-center font-semibold p-3 text-cyan-900 dark:text-cyan-200 px-6 
+    <div className="mt-10 bg-gradient-to-r from-pink-300 to-blue-800 p-1 
+    rounded-2xl baseline  dark:bg-gray-800 shadow-md shadow-blue-900 dark:shadow-blue-400">
+      <div className="flex justify-center items-center font-semibold p-3 text-cyan-900 dark:text-zinc-300 px-6 
       rounded-xl baseline dark:bg-gray-800 bg-blue-200">
         {value}
       </div>
